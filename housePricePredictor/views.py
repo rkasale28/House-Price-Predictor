f448098392b92cbb49c019be161fc260e4804565
rkasale28 <rohitkasale28@gmail.com>
2020-10-15 17:02:38 +0530

    Created Form

--- housePricePredictor/views.py
+++ housePricePredictor/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import pandas as pd
+
+data = {}
+amenities = ['Resale',  'MaintenanceStaff', 'Gymnasium', 'SwimmingPool', 'LandscapedGardens',
+'JoggingTrack', 'RainWaterHarvesting', 'IndoorGames', 'ShoppingMall',
+'Intercom', 'SportsFacility', 'ATM', 'ClubHouse', 'School',
+'24X7Security', 'PowerBackup', 'CarParking', 'StaffQuarter',
+'Cafeteria', 'MultipurposeRoom', 'Hospital', 'WashingMachine',
+'Gasconnection', 'AC', 'Wifi', 'Children\'splayarea', 'LiftAvailable',
+'BED', 'VaastuCompliant', 'Microwave', 'GolfCourse', 'TV',
+'DiningTable', 'Sofa', 'Wardrobe', 'Refrigerator']
 
 # Create your views here.
+def index(request):
+    data['amenities'] = amenities
+    return render(request,'index.html',data)
+
+def result(request):
+    if request.method=='POST':
+        data['amenities'] = amenities
+        data['city'] = request.POST['cities']
+        data['area'] = request.POST['area']
+        data['bedrooms'] = request.POST['bedrooms']
+
+        dict = {}
+        for i in amenities:
+            dict[i] = request.POST.__contains__(i.lower())
+        data['dict'] = dict
+        
+        return render(request,'result.html',data)
+
+    return HttpResponse('Not Found')
--- predictor/settings.py
+++ predictor/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'predictor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'predictor/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
